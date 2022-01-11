bd9c4b630e00343cd7972db5efa6bdc0eda35c9e
Sayeem2004 <MohammadSSKC@gmail.com>
2022-01-10 23:43:08 -0500

    update

--- data/script.js
+++ data/script.js
@@ -1,14 +1,9 @@
+// Written By Me
 // Functions For The Homepage
 function start_home() {
     // Setting Up CSS
     document.getElementById("body").style["background-image"] = "url(../images/home.jpg)";
-    document.getElementById("box").style["background-color"] = "#8f57ba";
-    document.getElementById("text").style["color"] = "white";
-    var buttons = document.getElementsByClassName("button");
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].style["background-color"] = "#8f57ba";
-        buttons[i].style["color"] = "white";
-    }
+    consoleText(["Welcome To NYX!", "Please click the button below to get started."], "text", ["#C3B1E1", "#C3B1E1"]);
 }
 
 function end_home() {
@@ -19,20 +14,7 @@ function end_home() {
 // Functions For Scene 1
 function start_scene1() {
     // Setting Up CSS
-    document.getElementById("body").style["background-image"] = "url(../images/scene1.png)";
-    var boxes = document.getElementsByClassName("box");
-    for (var i = 0; i < boxes.length; i++) {
-        boxes[i].style["background-color"] = "#307342";
-    }
-    var texts = document.getElementsByTagName("p");
-    for (var i = 0; i < texts.length; i++) {
-        texts[i].style["color"] = "white";
-    }
-    var buttons = document.getElementsByClassName("button");
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].style["background-color"] = "#307342";
-        buttons[i].style["color"] = "white";
-    }
+    document.getElementById("body").style["background-image"] = "url(../images/scene1.jpg)";
 }
 
 function update_scene1(type) {
