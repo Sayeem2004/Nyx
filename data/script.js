a84466bff0df516b3a56fc4fa3e94ee10522de7b
Sayeem2004 <MohammadSSKC@gmail.com>
2022-01-13 23:18:33 -0500

    made working buttons?

--- data/script.js
+++ data/script.js
@@ -1,12 +1,13 @@
 // Written By Me
 
 // Functions For The Homepage
+
 function start_home() {
     // Setting Up Background
     document.getElementById("body").style["background-image"] = "url(../images/home.jpg)";
 
     // Setting Up Variables
-    var speed = 75;
+    var speed = 50;
     var delay = 25*speed;
     var header = document.getElementById("header");
 
@@ -82,7 +83,7 @@ function start_scene1() {
     document.getElementById("body").style["background-image"] = "url(../images/scene1.jpg)";
 
     // Setting Up Variables
-    var speed = 75;
+    var speed = 50;
     var delay = 25*speed;
     var header = document.getElementById("header");
     var story = document.getElementById("story");
@@ -254,7 +255,26 @@ function getRandomInt(min, max) {
 function update(tag, dif, mn, mx) {
     tag.value = parseInt(tag.value) + dif;
     tag.value = Math.max(mn, parseInt(tag.value));
-    tag.value = Math.min(mx, praseInt(tag.value));
+    tag.value = Math.min(mx, parseInt(tag.value));
+}
+
+function update_scene1(mode) {
+    if (mode == 0) {
+        follow_msgs = ["", "", "", "", "", "You stand in front of a withered forest, should you enter?"];
+        follow_update(follow_msgs);
+    }
+    if (mode == 1) {
+        food_msgs = ["You have found and eaten a sugon shroom.", "You have found and eaten a ligon shroom.", "You have found and eaten a gulpon shroom.", "You have found and eaten a slurbon shroom."];
+        food_update(food_msgs);
+    }
+    if (mode == 2) {
+        water_msgs = ["You have drunken water from a nearby stream."];
+        water_update(water_msgs);
+    }
+    if (mode == 3) {
+        sleep_msgs = ["You have collapsed on the grass and taken a nap", "You have rested in the shade of a nearby tree", "You have slept under the blaring sun."];
+        sleep_update(sleep_msgs);
+    }
 }
 
 function follow_update(msgs) {
@@ -269,42 +289,301 @@ function follow_update(msgs) {
     var hunger = document.getElementById("current_hunger");
     var thirst = document.getElementById("current_thirst");
     var energy = document.getElementById("current_energy");
-    update(energy, getRandomInt(5, 11), 0, 100);
+    var health_label = document.getElementById("health_label");
+    var hunger_label = document.getElementById("hunger_label");
+    var thirst_label = document.getElementById("thirst_label");
+    var energy_label = document.getElementById("energy_label");
+    update(energy, -1*getRandomInt(5, 11), 0, 100);
     current_energy.style["width"] = current_energy.value + "%";
-    update(hunger, getRandomInt(5, 11), 0, 100);
+    energy_label.innerHTML = "Energy: " + current_energy.value + "%";
+    update(hunger, -1*getRandomInt(5, 11), 0, 100);
     current_hunger.style["width"] = current_hunger.value + "%";
-    update(thirst, getRandomInt(5, 16), 0, 100);
+    hunger_label.innerHTML = "Hunger: " + current_hunger.value + "%";
+    update(thirst, -1*getRandomInt(5, 16), 0, 100);
     current_thirst.style["width"] = current_thirst.value + "%";
+    thirst_label.innerHTML = "Thirst: " + current_thirst.value + "%";
 
     // Updating Warnings
-    var response = document.getElementById("response");
-    response.innerHTML = "";
+    var warning = document.getElementById("warning");
+    warning.style["color"] = "red";
+    warning.style["font-size"] = "20px";
+    warning.innerHTML = "";
     if (energy.value == 0) {
         update(health, -1*getRandomInt(3, 6), 0, 100);
         current_health.style["width"] = current_health.value + "%";
-        response.innerHTML += "You lost health from extreme tiredness. ";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme tiredness. ";
     }
     if (hunger.value == 0) {
         update(health, -1*getRandomInt(3, 11), 0, 100);
         current_health.style["width"] = current_health.value + "%";
-        response.innerHTML += "You lost health from extreme hunger. "
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme hunger. "
     }
     if (thirst.value == 0) {
         update(health, -1*getRandomInt(3, 16), 0, 100);
         current_health.style["width"] = current_health.value + "%";
-        response.innerHTML += "You lost health from extreme thirst. ";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme thirst. ";
     }
     if (health.value == 0) {
         start_death(1);
     }
 
     // Updating Response Count
-    var follow_count = document.getElementById("follow_count");
-    follow_count.value = parseInt(follow_count.value)+1;
-    response.innerHTML += msgs[Math.min(parseInt(follow_count.value), msgs.length-1)]
+    var response = document.getElementById("response");
+    response.style["color"] = "#17D4FE";
+    var count = document.getElementById("follow_count");
+    var cnt = parseInt(count.value)
+    response.innerHTML = msgs[Math.min(cnt, msgs.length-1)]
+    if (cnt == msgs.length+1) end_scene1();
+    count.value = cnt+1;
+}
 
-    // Changing Scene If Necessary
-    if (follow_count.value == msg.length+1) end_scene1();
+function food_update(msgs) {
+    // Variables
+    var health = document.getElementById("current_health");
+    var hunger = document.getElementById("current_hunger");
+    var thirst = document.getElementById("current_thirst");
+    var energy = document.getElementById("current_energy");
+    var health_label = document.getElementById("health_label");
+    var hunger_label = document.getElementById("hunger_label");
+    var thirst_label = document.getElementById("thirst_label");
+    var energy_label = document.getElementById("energy_label");
+
+    // Preventing Spam
+    var count = document.getElementById("food_count");
+    var cnt = parseInt(count.value);
+    count.value = cnt+1;
+    var warning = document.getElementById("warning");
+    warning.style["color"] = "red";
+    warning.style["font-size"] = "20px";
+    warning.innerHTML = "";
+    if (cnt >= 20) {
+        warning.innerHTML = "This place seems to have run out of food.";
+        update(energy, -1*getRandomInt(5, 11), 0, 100);
+        current_energy.style["width"] = current_energy.value + "%";
+        energy_label.innerHTML = "Energy: " + current_energy.value + "%";
+        update(hunger, -1*getRandomInt(5, 11), 0, 100);
+        current_hunger.style["width"] = current_hunger.value + "%";
+        hunger_label.innerHTML = "Hunger: " + current_hunger.value + "%";
+        update(thirst, -1*getRandomInt(5, 16), 0, 100);
+        current_thirst.style["width"] = current_thirst.value + "%";
+        thirst_label.innerHTML = "Thirst: " + current_thirst.value + "%";
+    } else {
+        // Updating Health, Food, Thirst, Energy
+        update(energy, -1*getRandomInt(5, 11), 0, 100);
+        current_energy.style["width"] = current_energy.value + "%";
+        energy_label.innerHTML = "Energy: " + current_energy.value + "%";
+        update(hunger, getRandomInt(25, 51), 0, 100);
+        current_hunger.style["width"] = current_hunger.value + "%";
+        hunger_label.innerHTML = "Hunger: " + current_hunger.value + "%";
+        update(thirst, -1*getRandomInt(5, 16), 0, 100);
+        current_thirst.style["width"] = current_thirst.value + "%";
+        thirst_label.innerHTML = "Thirst: " + current_thirst.value + "%";
+    }
+
+    // Updating Time
+    var time_add = getRandomInt(30, 76);
+    var clock = document.getElementById("current_time");
+    update(clock, time_add, 0, 1000000000);
+    clock.innerHTML = parse_time(parseInt(clock.value));
+
+    // Updating Warnings
+    if (energy.value == 0) {
+        update(health, -1*getRandomInt(3, 6), 0, 100);
+        current_health.style["width"] = current_health.value + "%";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme tiredness. ";
+    }
+    if (hunger.value == 0) {
+        update(health, -1*getRandomInt(3, 11), 0, 100);
+        current_health.style["width"] = current_health.value + "%";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme hunger. "
+    }
+    if (thirst.value == 0) {
+        update(health, -1*getRandomInt(3, 16), 0, 100);
+        current_health.style["width"] = current_health.value + "%";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme thirst. ";
+    }
+    if (health.value == 0) {
+        start_death(1);
+    }
+
+    // Updating Response Count
+    var response = document.getElementById("response");
+    response.style["color"] = "#17D4FE";
+    if (cnt < 20) {
+        response.innerHTML = msgs[getRandomInt(0, msgs.length)];
+    } else {
+        response.innerHTML = "";
+    }
+}
+
+function water_update(msgs) {
+    // Variables
+    var health = document.getElementById("current_health");
+    var hunger = document.getElementById("current_hunger");
+    var thirst = document.getElementById("current_thirst");
+    var energy = document.getElementById("current_energy");
+    var health_label = document.getElementById("health_label");
+    var hunger_label = document.getElementById("hunger_label");
+    var thirst_label = document.getElementById("thirst_label");
+    var energy_label = document.getElementById("energy_label");
+
+    // Preventing Spam
+    var count = document.getElementById("water_count");
+    var cnt = parseInt(count.value);
+    count.value = cnt+1;
+    var warning = document.getElementById("warning");
+    warning.style["color"] = "red";
+    warning.style["font-size"] = "20px";
+    warning.innerHTML = "";
+    if (cnt >= 20) {
+        warning.innerHTML = "This place seems to have run out of water.";
+        update(energy, -1*getRandomInt(5, 11), 0, 100);
+        current_energy.style["width"] = current_energy.value + "%";
+        energy_label.innerHTML = "Energy: " + current_energy.value + "%";
+        update(hunger, -1*getRandomInt(5, 11), 0, 100);
+        current_hunger.style["width"] = current_hunger.value + "%";
+        hunger_label.innerHTML = "Hunger: " + current_hunger.value + "%";
+        update(thirst, -1*getRandomInt(5, 16), 0, 100);
+        current_thirst.style["width"] = current_thirst.value + "%";
+        thirst_label.innerHTML = "Thirst: " + current_thirst.value + "%";
+    } else {
+        // Updating Health, Food, Thirst, Energy
+        update(energy, -1*getRandomInt(5, 11), 0, 100);
+        current_energy.style["width"] = current_energy.value + "%";
+        energy_label.innerHTML = "Energy: " + current_energy.value + "%";
+        update(hunger, -1*getRandomInt(5, 11), 0, 100);
+        current_hunger.style["width"] = current_hunger.value + "%";
+        hunger_label.innerHTML = "Hunger: " + current_hunger.value + "%";
+        update(thirst, getRandomInt(25, 51), 0, 100);
+        current_thirst.style["width"] = current_thirst.value + "%";
+        thirst_label.innerHTML = "Thirst: " + current_thirst.value + "%";
+    }
+
+    // Updating Time
+    var time_add = getRandomInt(30, 76);
+    var clock = document.getElementById("current_time");
+    update(clock, time_add, 0, 1000000000);
+    clock.innerHTML = parse_time(parseInt(clock.value));
+
+    // Updating Warnings
+    if (energy.value == 0) {
+        update(health, -1*getRandomInt(3, 6), 0, 100);
+        current_health.style["width"] = current_health.value + "%";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme tiredness. ";
+    }
+    if (hunger.value == 0) {
+        update(health, -1*getRandomInt(3, 11), 0, 100);
+        current_health.style["width"] = current_health.value + "%";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme hunger. "
+    }
+    if (thirst.value == 0) {
+        update(health, -1*getRandomInt(3, 16), 0, 100);
+        current_health.style["width"] = current_health.value + "%";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme thirst. ";
+    }
+    if (health.value == 0) {
+        start_death(1);
+    }
+
+    // Updating Response Count
+    var response = document.getElementById("response");
+    response.style["color"] = "#17D4FE";
+    if (cnt < 20) {
+        response.innerHTML = msgs[getRandomInt(0, msgs.length)];
+    } else {
+        response.innerHTML = "";
+    }
+}
+
+function sleep_update(msgs) {
+    // Variables
+    var health = document.getElementById("current_health");
+    var hunger = document.getElementById("current_hunger");
+    var thirst = document.getElementById("current_thirst");
+    var energy = document.getElementById("current_energy");
+    var health_label = document.getElementById("health_label");
+    var hunger_label = document.getElementById("hunger_label");
+    var thirst_label = document.getElementById("thirst_label");
+    var energy_label = document.getElementById("energy_label");
+
+    // Preventing Spam
+    var count = document.getElementById("sleep_count");
+    var cnt = parseInt(count.value);
+    count.value = cnt+1;
+    var warning = document.getElementById("warning");
+    warning.style["color"] = "red";
+    warning.style["font-size"] = "20px";
+    warning.innerHTML = "";
+    if (cnt >= 20) {
+        warning.innerHTML = "You are too energetic to sleep.";
+        update(energy, -1*getRandomInt(5, 11), 0, 100);
+        current_energy.style["width"] = current_energy.value + "%";
+        energy_label.innerHTML = "Energy: " + current_energy.value + "%";
+        update(hunger, -1*getRandomInt(5, 11), 0, 100);
+        current_hunger.style["width"] = current_hunger.value + "%";
+        hunger_label.innerHTML = "Hunger: " + current_hunger.value + "%";
+        update(thirst, -1*getRandomInt(5, 16), 0, 100);
+        current_thirst.style["width"] = current_thirst.value + "%";
+        thirst_label.innerHTML = "Thirst: " + current_thirst.value + "%";
+    } else {
+        // Updating Health, Food, Thirst, Energy
+        update(energy, getRandomInt(50, 101), 0, 100);
+        current_energy.style["width"] = current_energy.value + "%";
+        energy_label.innerHTML = "Energy: " + current_energy.value + "%";
+        update(hunger, -1*getRandomInt(5, 11), 0, 100);
+        current_hunger.style["width"] = current_hunger.value + "%";
+        hunger_label.innerHTML = "Hunger: " + current_hunger.value + "%";
+        update(thirst, -1*getRandomInt(5, 16), 0, 100);
+        current_thirst.style["width"] = current_thirst.value + "%";
+        thirst_label.innerHTML = "Thirst: " + current_thirst.value + "%";
+    }
+
+    // Updating Time
+    var time_add = getRandomInt(360, 481);
+    var clock = document.getElementById("current_time");
+    update(clock, time_add, 0, 1000000000);
+    clock.innerHTML = parse_time(parseInt(clock.value));
+
+    // Updating Warnings
+    if (energy.value == 0) {
+        update(health, -1*getRandomInt(3, 6), 0, 100);
+        current_health.style["width"] = current_health.value + "%";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme tiredness. ";
+    }
+    if (hunger.value == 0) {
+        update(health, -1*getRandomInt(3, 11), 0, 100);
+        current_health.style["width"] = current_health.value + "%";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme hunger. "
+    }
+    if (thirst.value == 0) {
+        update(health, -1*getRandomInt(3, 16), 0, 100);
+        current_health.style["width"] = current_health.value + "%";
+        health_label.innerHTML = "Health: " + current_health.value + "%";
+        warning.innerHTML += "You lost health from extreme thirst. ";
+    }
+    if (health.value == 0) {
+        start_death(1);
+    }
+
+    // Updating Response Count
+    var response = document.getElementById("response");
+    response.style["color"] = "#17D4FE";
+    if (cnt < 20) {
+        response.innerHTML = msgs[getRandomInt(0, msgs.length)];
+    } else {
+        response.innerHTML = "";
+    }
 }
 
 function end_scene1() {
@@ -362,7 +641,7 @@ function continue_death() {
     document.getElementById("body").style["background-image"] = "url(../images/scene1.jpg)";
 
     // Declaring Variables
-    var speed = 75;
+    var speed = 50;
     var delay = 25*speed;
 
     // Giving Death Response
